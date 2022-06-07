63811e47ddfaee48a3098d06e2048e071b1939b7
Vaia Sanu <vaia.sanu@yahoo.com>
2022-06-07 17:25:33 +0700

    tampilan login (css) vaia

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react'
-import Button from '../UI/Button'
-import Something from '../../asset/Something.png';
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Something from "../../asset/Something.png";
 
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
 const Header = () => {
-
-
-
-
-    return (
-        <React.Fragment>
-            <div>
-                <img style={{maxHeight:"250%", maxWidth:"200px"}} src={Something}></img>
-            </div>
-            {/* <Button onClick={LoginFormHandler}></Button> */}
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div>
+        <img
+          style={{ maxWidth: "225px", marginTop: "-3rem", marginLeft: "2rem" }}
+          src={Something}
+        ></img>
+      </div>
+      {/* <Button onClick={LoginFormHandler}></Button> */}
+    </React.Fragment>
+  );
+};
 
 export default Header;
--- src/component/LoginForm/LoginForm.js
+++ src/component/LoginForm/LoginForm.js
@@ -1,12 +1,11 @@
 import useInput from "../../hooks/useInput";
 import Card from "../UI/Card";
-
-import './LoginForm.css'
+import Logo from "../../asset/UserLogo.png";
+import "./LoginForm.css";
 
 const LoginForm = (props) => {
-
-  const isNotEmpty = (value) => value.trim() !== ('');
-  const isEmail = (value) => value.includes('@');
+  const isNotEmpty = (value) => value.trim() !== "";
+  const isEmail = (value) => value.includes("@");
 
   const {
     value: passwordValue,
@@ -14,8 +13,8 @@ const LoginForm = (props) => {
     isValid: passwordIsValid,
     inputBlurHandler: passwordBlurHandler,
     valueChangeHandler: passwordChangeHandler,
-    reset: passwordResetHandler
-  } = useInput(isNotEmpty)
+    reset: passwordResetHandler,
+  } = useInput(isNotEmpty);
 
   const {
     value: EmailValue,
@@ -23,16 +22,12 @@ const LoginForm = (props) => {
     isValid: emailIsValid,
     inputBlurHandler: emailBlurHandler,
     valueChangeHandler: emailChangeHandler,
-    reset: emailResetHandler
-  } = useInput(isEmail)
-
+    reset: emailResetHandler,
+  } = useInput(isEmail);
 
   let formIsValid = false;
 
-  if (passwordIsValid && emailIsValid)
-    formIsValid = true;
-
-
+  if (passwordIsValid && emailIsValid) formIsValid = true;
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -40,53 +35,63 @@ const LoginForm = (props) => {
     if (!formIsValid) {
       return;
     }
-    console.log('submitted!')
-    console.log(passwordValue, EmailValue)
+    console.log("submitted!");
+    console.log(passwordValue, EmailValue);
     passwordResetHandler();
     emailResetHandler();
-
-  }
+  };
 
   const passwordClasses = passwordHasError
-    ? 'form-control invalid'
-    : 'form-control'
+    ? "form-control invalid"
+    : "form-control";
 
-  const emailClasses = emailHasError
-    ? 'form-control invalid'
-    : 'form-control'
-
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
 
   return (
     <Card>
-      <form onSubmit={submitHandler}>
-        <div className='control-group'>
+      <form className="form-layout" onSubmit={submitHandler}>
+        <img
+          style={{
+            maxWidth: "225px",
+            alignSelf: "center",
+            padding: "2rem 0",
+          }}
+          src={Logo}
+        ></img>
+        <div className="control-group">
           <div className={emailClasses}>
-            <label htmlFor='name'>Email</label>
+            <label htmlFor="name">Email</label>
             <input
-              type='text'
-              id='name'
+              type="email"
+              id="name"
               onChange={emailChangeHandler}
               onBlur={emailBlurHandler}
               value={EmailValue}
               required
             />
-            {emailHasError && <p className="error-text">Please enter your Email</p>}
+            {emailHasError && (
+              <p className="error-text">Please enter your Email</p>
+            )}
           </div>
           <div className={passwordClasses}>
-            <label htmlFor='name'>Password</label>
+            <label htmlFor="name">Password</label>
             <input
-              type='text'
-              id='name'
+              type="text"
+              id="name"
               onChange={passwordChangeHandler}
               onBlur={passwordBlurHandler}
               value={passwordValue}
               required
             />
-            {passwordHasError && <p className="error-text">Please enter your Password</p>}
+            {passwordHasError && (
+              <p className="error-text">Please enter your Password</p>
+            )}
           </div>
         </div>
-        <div className='form-actions'>
-          <button disabled={!formIsValid}>Masuk</button>
+        <div className="form-actions">
+          <button style={{ marginTop: "1rem" }} disabled={!formIsValid}>
+            Masuk
+          </button>
         </div>
       </form>
     </Card>
