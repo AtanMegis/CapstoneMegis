b738f5854af2c4f6c8043c1672a49741d0bcad7b
Vaia Sanu <vaia.sanu@yahoo.com>
2022-06-09 20:50:34 +0700

    push lagi lagi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LoginForm from "./component/LoginForm/LoginForm";
 import Header from "./component/Header/Header";
 import ProductList from "./component/Products/ProductsList";
+import Carousel from "./component/Header/Carousel/Carousel";
 
 function App() {
   const [LoginIsShown, setLoginIsShown] = useState(false);
@@ -20,6 +21,7 @@ function App() {
         {LoginIsShown && <LoginForm onClose={hideLoginHandler} />}
         <Header onShowLoginForm={showLoginForm} />
         <main>
+          <Carousel />
           <ProductList />
         </main>
         <footer></footer>
